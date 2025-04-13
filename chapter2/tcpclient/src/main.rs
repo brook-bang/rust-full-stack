ca25cc7ca694bf4418b812ce10c59b8ac2f67698
brook-bang <1455047806@qq.com>
2025-04-13 19:29:00 +0800

    start chapter2

---
+++ chapter2/tcpclient/src/main.rs
@@ -0,0 +1,16 @@
+use core::str;
+use std::{
+    io::{Read, Write},
+    net::TcpStream,
+};
+
+fn main() {
+    let mut stream = TcpStream::connect("127.0.0.1:3000").unwrap();
+    stream.write("hello".as_bytes()).unwrap();
+    let mut buffer = [0; 5];
+    stream.read(&mut buffer).unwrap();
+    println!(
+        "Got response from server: {:?}",
+        str::from_utf8(&buffer).unwrap()
+    );
+}
